5fea3b0cf2b2314e2cced9bd0e855fa63bb229aa
Adam Wagh <adamwagh@gmail.com>
2022-07-26 18:39:17 -0500

    still testing

--- __tests__/employee.test.js
+++ __tests__/employee.test.js
@@ -1,15 +1,29 @@
 const Employee = require('../lib/Employee')
+// describe ('Employee', () => {
+//     describe()
+// })
 // * `name`
     describe('employee.name', () => {
         it("should display the employee's name", () => {
-            const name = 'adam'
-            expect(Employee.name).toBe('Adam')
+            const employee = new Employee('Adam', '001', 'adam@me.com')
+            expect(employee.name).toEqual('Adam')
         })
     })
         
 // * `id`
-
+describe('employee.id', () => {
+    it("should display the employee's id", () => {
+        const employee = new Employee('Adam', '001', 'adam@me.com')
+        expect(employee.id).toEqual('001')
+    })
+})
 // * `email`
+describe('employee.email', () => {
+    it("should display the employee's email", () => {
+        const employee = new Employee('Adam', '001', 'adam@me.com')
+        expect(employee.email).toEqual('adam@me.com')
+    })
+})
 
 // * `getName()`
 
